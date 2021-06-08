0583db3dc7a3ddee3955bbeccdacbdcbc3170a82
Tetiana Yarova <andronikova11@gmail.com>
2021-06-08 22:16:29 +0300

    Создает refs

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+  searchForm: document.querySelector('.js-search-form'),
+  galleryContainer: document.querySelector('.js-gallery-container'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+
+export default refs;
