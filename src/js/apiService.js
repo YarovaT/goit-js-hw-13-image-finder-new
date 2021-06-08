2604898e01d6ee83d3f5bdae431dafcb71939b4c
Tetiana Yarova <andronikova11@gmail.com>
2021-06-08 22:15:58 +0300

    Добавляет класс-конструктор API

---
+++ src/js/apiService.js
@@ -0,0 +1,36 @@
+const API_KEY = '21973374-56c885108110b7a34a7b7e2cd';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchGalleryCards() {
+    const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&${this.page}&per_page=12&key=${API_KEY}`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
